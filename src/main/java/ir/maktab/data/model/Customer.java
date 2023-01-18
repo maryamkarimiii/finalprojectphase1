9e8dc7049f69a029f66493a903cdd5e44eee427c
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-01-18 17:25:43 +0330

    add Customer class into model pack

---
+++ src/main/java/ir/maktab/data/model/Customer.java
@@ -0,0 +1,11 @@
+package ir.maktab.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+public class Customer extends User {
+//    @OneToMany
+//    Set<Address> address;
+}
