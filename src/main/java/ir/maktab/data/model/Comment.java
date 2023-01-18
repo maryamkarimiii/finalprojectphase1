f1b44e3514f6b21f5d960321ff64a826d9d8e271
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-01-18 17:21:13 +0330

    add Comment class into model pack

---
+++ src/main/java/ir/maktab/data/model/Comment.java
@@ -0,0 +1,22 @@
+package ir.maktab.data.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @OneToOne
+    Expert expert;
+    @OneToOne
+    Customer customer;
+    Integer score;
+    String comment;
+}
