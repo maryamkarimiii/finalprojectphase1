98ad65016a41b77bfc88787ce7dd4921dc34dfb2
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-01-18 17:38:08 +0330

    add SubService class into model pack

---
+++ src/main/java/ir/maktab/data/model/SubService.java
@@ -0,0 +1,23 @@
+package ir.maktab.data.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class SubService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Long baseAmount;
+    String description;
+    @ManyToOne
+    Service service;
+    @Column(columnDefinition = "boolean default false")
+    boolean isDeleted;
+}
