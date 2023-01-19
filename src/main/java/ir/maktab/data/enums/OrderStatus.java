d5556cb55139b7eb032375d93c81950d48d2073f
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-01-19 13:32:48 +0330

    change order status and delete constructore

--- src/main/java/ir/maktab/data/enums/OrderStatus.java
+++ src/main/java/ir/maktab/data/enums/OrderStatus.java
@@ -3,24 +3,10 @@ package ir.maktab.data.enums;
 import java.util.Date;
 
 public enum OrderStatus {
-    WAITING_FOR_EXPERT_RECOMMENDATION(new Date()),
-    WAITING_FOR_CUSTOMER_CHOICE(new Date()),
-    WAITING_FOR_EXPERT_COME(new Date()),
-    START_WORKING(new Date()),
-    FINISH_WORKING(new Date()),
-    PAID(new Date());
-
-    OrderStatus(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    private Date startTime;
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getStartTime() {
-        return this.startTime;
-    }
+    WAITING_FOR_EXPERT_RECOMMENDATION(),
+    WAITING_FOR_CUSTOMER_CHOICE(),
+    WAITING_FOR_EXPERT_COME(),
+    START_WORKING(),
+    FINISH_WORKING(),
+    PAID();
 }
